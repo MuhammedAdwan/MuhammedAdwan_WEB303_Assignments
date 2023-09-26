bf5ebde40411ab1da64261ddb5df388e9f11dcfd
MuhammedAdwan <133061822+MuhammedAdwan@users.noreply.github.com>
2023-09-26 17:55:00 -0400

    updating the assignment 2 with the instructor

---
+++ js/class.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("#prospect").on("click", function(){
+        var xhr = new XMLHttpRequest;
+        xhr.open("GET", "prospect.html", true);
+        xhr.onload = function () {
+            if (xhr.status === 200)
+            $("#content").html(xhr.responseText).hide().animate({fontsize: "1.5em", 1000},)fadeIn(1000);
+        }
+    }
+    xhr.send();
+    });
+});
