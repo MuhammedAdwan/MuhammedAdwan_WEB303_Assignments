d0992bf58c9883547368ee35d6b6376d9e13ef9a
MuhammedAdwan <133061822+MuhammedAdwan@users.noreply.github.com>
2023-09-27 17:20:30 -0400

    class work

--- js/class.js
+++ js/class.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $("#prospect").on("click", function(){
-        var xhr = new XMLHttpRequest;
+        var xhr = new XMLHttpRequest();
         xhr.open("GET", "prospect.html", true);
         xhr.onload = function () {
-            if (xhr.status === 200)
-            $("#content").html(xhr.responseText).hide().animate({fontsize: "1.5em", 1000},)fadeIn(1000);
-        }
-    }
-    xhr.send();
+            if (xhr.status === 200) {
+                $("#content").html(xhr.responseText).hide().animate({fontSize: "1.5em"}, 1000).fadeIn(1000);
+            }
+        };
+        xhr.send();
     });
 });
