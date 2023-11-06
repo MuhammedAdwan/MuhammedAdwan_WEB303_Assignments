84605a24030238f4f9f166aff0149e1b2d29ab71
MuhammedAdwan <133061822+MuhammedAdwan@users.noreply.github.com>
2023-11-06 10:32:39 -0500

    change the idname

--- WEB303As07/js/script.js
+++ WEB303As07/js/script.js
@@ -1,18 +1,18 @@
-$(function () {
-    $('#photo-viewer').customPhotoViewer().show().on('click', '.photo-box', function (e) {
-        var $content = $(this).clone().find('img').css({
-            marginLeft: 0,
-            marginTop: 0,
-            width: '100%',
-            height: 'auto'
-        });
-        // Modal code goes here
-        $('#myModal').css('display', 'block');
-        $('#myModal img').attr('src', $content.attr('src'));
-    });
-
-    // Close the modal when the close button is clicked
-    $('.close').click(function () {
-        $('#myModal').css('display', 'none');
-    });
-});
+$('.thumbnail-anchor').on('click', function(e) {
+    e.preventDefault();
+    
+    var imgSrc = $(this).attr('href');
+    
+    $('#modal img').attr('src', imgSrc);
+    
+    $('.active').removeClass('active');
+    $(this).addClass('active');
+    
+    $('#modal').show();
+    
+  });
+  
+  // Hide modal on close click  
+  $('#modal').on('click', function() {
+    $(this).hide();
+  })
