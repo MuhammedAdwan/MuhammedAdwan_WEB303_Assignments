a4b6737063818e141c126c98759482d90ad7b490
MuhammedAdwan <133061822+MuhammedAdwan@users.noreply.github.com>
2023-11-07 12:47:20 -0500

    Update script.js

--- WEB303As07/js/script.js
+++ WEB303As07/js/script.js
@@ -1,18 +1,32 @@
-$('.thumbnail-anchor').on('click', function(e) {
+$(document).ready(function () {
+  $('.thumbnail-anchor').on('click', function (e) {
     e.preventDefault();
-    
+
     var imgSrc = $(this).attr('href');
-    
-    $('#modal img').attr('src', imgSrc);
-    
+
+    // Set the size of the loading section
+    $('.photo-box').css({
+      width: '840px',
+      height: '400px',
+    });
+
+    // Update the loading section image source and size
+    $('.photo-box').removeClass('is-loading');
+    $('.photo-box').css('background-image', 'url(' + imgSrc + ')');
+
+    // Show the loading section
+    $('.photo-box').addClass('is-loading');
+
+    // Hide the modal window
+    $('#modal').hide();
+
+    // Highlight the active thumbnail
     $('.active').removeClass('active');
     $(this).addClass('active');
-    
-    $('#modal').show();
-    
   });
-  
-  // Hide modal on close click  
-  $('#modal').on('click', function() {
-    $(this).hide();
-  })
+
+  // Hide the loading section on close click
+  $('.close').on('click', function () {
+    $('.photo-box').removeClass('is-loading');
+  });
+});
