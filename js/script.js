e58143d12b587696b86de9a304f5836e5a7e8b4f
MuhammedAdwan <133061822+MuhammedAdwan@users.noreply.github.com>
2023-12-03 15:12:39 -0500

    updateed the index file and made the form using js instead of html

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let form = document.getElementById("registrationForm");
-    let usernameInput = document.getElementById("username");
-    let passwordInput = document.getElementById("password");
-    let password2Input = document.getElementById("password2");
-    let termsCheckbox = document.getElementById("terms");
-    let countrySelect = document.getElementById("country");
-    let submitButton = document.getElementById("submit");
-    let welcomeMessage = document.getElementById("welcomeMessage");
-    let passwordMatchMessage = document.getElementById("passwordMatchMessage");
+    // Create form and elements
+    let form = document.createElement("form");
+    form.id = "registrationForm";
 
-    // this code will populate the countries in countries.js
+    let usernameLabel = createLabel("Enter Username:");
+    let usernameInput = createInput("text", "username", true);
+
+    let passwordLabel = createLabel("Enter Password (at least 12 characters):");
+    let passwordInput = createInput("password", "password", false);
+
+    let password2Label = createLabel("Confirm your Password:");
+    let password2Input = createInput("password", "password2", false);
+    let passwordMatchMessage = document.createElement("span");
+    passwordMatchMessage.id = "passwordMatchMessage";
+
+    let termsLabel = createLabel("I accept the terms of service:");
+    let termsInput = createInput("checkbox", "terms", false, "yes");
+
+    let countryLabel = createLabel("Select your country:");
+    let countrySelect = document.createElement("select");
+    countrySelect.id = "country";
+    countrySelect.name = "country";
     const defaultOption = document.createElement("option");
     defaultOption.value = "";
     defaultOption.text = "Select your country";
     countrySelect.appendChild(defaultOption);
-
     countries.forEach(function (country) {
         const option = document.createElement("option");
         option.value = country.code;
@@ -22,11 +32,36 @@ document.addEventListener("DOMContentLoaded", function () {
         countrySelect.appendChild(option);
     });
 
+    let submitButton = createInput("submit", "submit", true, "Submit");
+    submitButton.disabled = true;
+
+    let welcomeMessage = document.createElement("p");
+    welcomeMessage.id = "welcomeMessage";
+    welcomeMessage.style.display = "none";
+
+    // Append elements to the form
+    form.appendChild(usernameLabel);
+    form.appendChild(usernameInput);
+    form.appendChild(passwordLabel);
+    form.appendChild(passwordInput);
+    form.appendChild(password2Label);
+    form.appendChild(password2Input);
+    form.appendChild(passwordMatchMessage);
+    form.appendChild(termsLabel);
+    form.appendChild(termsInput);
+    form.appendChild(countryLabel);
+    form.appendChild(countrySelect);
+    form.appendChild(submitButton);
+    form.appendChild(welcomeMessage);
+
+    // Append form to the body
+    document.body.appendChild(form);
+
     // Event listeners for form controls
     usernameInput.addEventListener("input", validateForm);
     passwordInput.addEventListener("input", validateForm);
     password2Input.addEventListener("input", validateForm);
-    termsCheckbox.addEventListener("change", validateForm);
+    termsInput.addEventListener("change", validateForm);
     countrySelect.addEventListener("change", validateForm);
 
     form.addEventListener("submit", function (event) {
@@ -36,14 +71,38 @@ document.addEventListener("DOMContentLoaded", function () {
         var selectedCountryCode = countrySelect.value;
         welcomeMessage.innerText = `Welcome ${usernameInput.value}! The country code you selected is ${selectedCountryCode}.`;
         welcomeMessage.style.display = "block";
+
     });
+
+    // Function to create label
+    function createLabel(text) {
+        let label = document.createElement("label");
+        label.htmlFor = text.toLowerCase().replace(/\s/g, "");
+        label.innerText = text;
+        return label;
+    }
+
+    // Function to create input element
+    function createInput(type, id, required, value) {
+        let input = document.createElement("input");
+        input.type = type;
+        input.id = id;
+        input.name = id;
+        if (required) {
+            input.required = true;
+        }
+        if (value) {
+            input.value = value;
+        }
+        return input;
+    }
 
     // Function to validate the form and enable/disable the submit button
     function validateForm() {
         let isUsernameValid = usernameInput.value.trim() !== "";
         let isPasswordValid = passwordInput.value.length >= 12;
         let isPasswordMatch = passwordInput.value === password2Input.value;
-        let isTermsChecked = termsCheckbox.checked;
+        let isTermsChecked = termsInput.checked;
         let isCountrySelected = countrySelect.value !== "";
 
         submitButton.disabled = !(isUsernameValid && isPasswordValid && isPasswordMatch && isTermsChecked && isCountrySelected);
